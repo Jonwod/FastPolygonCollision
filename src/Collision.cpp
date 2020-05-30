9e030a1ce9288c26333b3d33218f78bd3e057f56
Jonathan Wood <jonathan.m.wood95@gmail.com>
2020-05-30 16:13:14 +0100

    Minor optimization

--- src/Collision.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by jon on 02/08/2019.
-//
-
-#include "Collision.h"
-
-
--- src/Collision.h
+++ src/Collision.h
@@ -25,18 +25,20 @@ inline bool areColliding(int polyASize, const V* polyA, int polyBSize, const V* 
 
         const V edgeNormal = perpendicularRight(edge1.end - edge1.start);
 
-        float a_min = std::numeric_limits<float>::infinity();
-        float a_max = -std::numeric_limits<float>::infinity();
-        for(int vert_i = 0; vert_i < polyASize; ++vert_i) {
+        const float firstAVertProj = Vec2::dotProduct(edgeNormal, polyA[0]);
+        float a_min = firstAVertProj;
+        float a_max = firstAVertProj;
+        for(int vert_i = 1; vert_i < polyASize; ++vert_i) {
             const V& vert = polyA[vert_i];
             const float vertProj = Vec2::dotProduct(edgeNormal, vert);
             if(vertProj < a_min) { a_min = vertProj; }
             if(vertProj > a_max) { a_max = vertProj; }
         }
 
-        float b_min = std::numeric_limits<float>::infinity();
-        float b_max = -std::numeric_limits<float>::infinity();
-        for(int vert_i = 0; vert_i < polyBSize; ++vert_i) {
+        const float firstBVertProj = Vec2::dotProduct(edgeNormal, polyB[0]);
+        float b_min = firstBVertProj;
+        float b_max = firstBVertProj;
+        for(int vert_i = 1; vert_i < polyBSize; ++vert_i) {
             const V& vert = polyB[vert_i];
             const float vertProj = Vec2::dotProduct(edgeNormal, vert);
             if(vertProj < b_min) { b_min = vertProj; }
